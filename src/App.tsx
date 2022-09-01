a764d563cf71c23f60aeede395d1f42202a1122c
joaogkvalho <joaogkvalho@gmail.com>
2022-09-01 12:46:16 -0300

    first module finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Button } from './components/Button'
+
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
